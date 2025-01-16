cc512147b11e6ea6ee6b2d6459d72521a83338ee
wcamer <xeus.xiii@hotmail.com>
2025-01-16 01:44:10 -0700

    Model structures for data in database

---
+++ DAS/Models/DoctorNote.cs
@@ -0,0 +1,46 @@
+using System.ComponentModel.DataAnnotations;
+// using CarServiceScheduler.Components.Pages;
+
+namespace DAS.Models;
+
+public class DoctorNote
+{
+    public required int DoctorNoteId { get; set; }
+
+    [DataType(DataType.Date)]
+    public required DateTime Date { get; set; }
+
+    public required int PatientId { get; set; }
+
+    public required int DoctorId { get; set; }
+
+    [RegularExpression(@"^[a-zA-z]*$",
+    ErrorMessage = "Patient name must be at least 2 letters but no more than 40 letters long")]
+    [StringLength(40, MinimumLength = 2)]
+    [Required]
+    public required string PatientName { get; set; }
+
+    [RegularExpression(@"^[a-zA-z]*$",
+    ErrorMessage = "The Doctor name must be at least 2 letters but no more than 40 letters long")]
+    [StringLength(40, MinimumLength = 2)]
+    [Required]
+    public required string DoctorName { get; set; }
+
+    [RegularExpression(@"^[a-zA-Z0-9\s]*$",
+    ErrorMessage = "Note contents must only contain letters and numbers, have a min length of 2, and a max length of 1000")]
+    [Required]
+    [StringLength(1000, MinimumLength = 2)]
+    public required string Note { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ DAS/Models/Profile.cs
@@ -0,0 +1,62 @@
+using System.ComponentModel.DataAnnotations;
+// using CarServiceScheduler.Components.Pages;
+
+namespace DAS.Models;
+
+public class Profile
+{
+
+    public required int ProfileId { get; set; }
+
+    [RegularExpression(@"^[a-zA-z]*$",
+    ErrorMessage = "First name must be at least 2 letters but no more than 20 letters long")]
+    [StringLength(20, MinimumLength = 2)]
+    [Required]
+    public required string FirstName { get; set; }
+
+    [RegularExpression(@"^[a-zA-z]*$")]
+    [Required]
+    [StringLength(20, MinimumLength = 2,
+    ErrorMessage = "Last name must be at least 2 letters long but no more than 20 letters")]
+    public required string LastName { get; set; }
+
+    [RegularExpression(@"^[0-9]+$")]
+    [Required]
+    [StringLength(10, MinimumLength = 10,
+    ErrorMessage = "Please enter your 10 digit telephone with only the numbers and no dashes")]
+    public required string Telephone { get; set; }
+
+
+    [RegularExpression(@"^[a-zA-Z0-9\s]*$",
+    ErrorMessage = "Address must only contain letters and numbers, have a min length of 2, and a max length of 50")]
+    [Required]
+    [StringLength(50, MinimumLength = 2)]
+    public required string Address { get; set; }
+
+
+    [RegularExpression(@"^[a-zA-Z0-9\s]*$",
+    ErrorMessage = "City name must contain letters or numbers, min length of 2 and max length of 20")]
+    [Required]
+    [StringLength(20, MinimumLength = 2)]
+    public required string City { get; set; }
+
+
+    [RegularExpression(@"^[a-zA-Z0-9\s]*$",
+    ErrorMessage = "State name must contain letters and/or numbers with a min length of 2 and max length of 20")]
+    [Required]
+    [StringLength(20, MinimumLength = 2)]
+    public required string State { get; set; }
+
+
+    [Required]
+    [StringLength(15, MinimumLength = 2,
+    ErrorMessage = "ZipCode code must have at least a length of 2 but no more than 15")]
+    public required string ZipCode { get; set; }
+
+
+
+    public string? Email { get; set; }
+
+    public string? Password { get; set; }
+
+}
