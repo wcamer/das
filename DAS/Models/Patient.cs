0d3760ce19834a2cbd6f3706f4a86d2871be0a1e
wcamer <xeus.xiii@hotmail.com>
2025-01-17 21:52:17 -0700

    Changed some model attributes to accept blank strings in case model does not need insurance info and made patientGroupId nullable if ever used.

--- DAS/Models/Patient.cs
+++ DAS/Models/Patient.cs
@@ -12,7 +12,7 @@ public class Patient
     [Required]
     public int ProfileId { get; set; }  // this is a foreign key to a specific profile
 
-    // public int PatientGroupId { get; set; } // this is here if a particular feature is built
+    public int? PatientGroupId { get; set; } // this is here if a particular feature is built
 
 
     [RegularExpression(@"^[a-zA-Z\s]*$",
@@ -28,14 +28,14 @@ public class Patient
     public required string LastName { get; set; } = string.Empty;
 
     [RegularExpression(@"^[a-zA-Z\s]*$",
-    ErrorMessage = "Please enter the insurance company name that is at least 2 letters long and no more than 40 letters total")]
-    [StringLength(40, MinimumLength = 2)]
-    public string InsuranceCompany { get; set; } = string.Empty;
+    ErrorMessage = "Please enter the insurance company name that is no more than 40 letters total")]
+    [StringLength(40, MinimumLength = 0)]
+    public string? InsuranceCompany { get; set; }
 
     [RegularExpression(@"^[a-zA-Z0-9\s]*$",
     ErrorMessage = "Please enter insurance policy number that is at least 1 character long and no more than 10 characters total")]
-    [StringLength(10, MinimumLength = 1)]
-    public string InsurancePolicyNumber { get; set; } = string.Empty;
+    [StringLength(10, MinimumLength = 0)]
+    public string? InsurancePolicyNumber { get; set; }
 
 
 
