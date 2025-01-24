a9dd859ef563c41e8e4ebef8e0d7009b3649c9c3
wcamer <xeus.xiii@hotmail.com>
2025-01-23 19:43:25 -0700

    Included authentication and password hashing for Service Providers group

--- DAS/AuthService.cs
+++ DAS/AuthService.cs
@@ -6,36 +6,82 @@ using Microsoft.EntityFrameworkCore;
 public class AuthService
 {
     private readonly DASContext _dbContext;
-    private readonly IPasswordHasher<Profile> _passwordHasher;
+    private readonly IPasswordHasher<Profile> _PatientPasswordHasher;
+    private readonly IPasswordHasher<ServiceProviders> _SPPasswordHasher;
 
-    public AuthService(DASContext context, IPasswordHasher<Profile> passwordHasher)
+    public AuthService(DASContext context, IPasswordHasher<Profile> PatientPasswordHasher, IPasswordHasher<ServiceProviders> SPPasswordHasher)
     {
         _dbContext = context;
-        _passwordHasher = passwordHasher;
+        _PatientPasswordHasher = PatientPasswordHasher;
+        _SPPasswordHasher = SPPasswordHasher;
     }
 
     public async Task<User?> LoginAsync(string email, string password)
     {
         var profile = await _dbContext.Profiles.FirstOrDefaultAsync(p => p.Email == email);
-        if (profile == null) return null;
-
-        // Verify password
-        var result = _passwordHasher.VerifyHashedPassword(profile, profile.Password ?? "", password);
-        if (result == PasswordVerificationResult.Success)
+        if (profile == null)
         {
-            return new User
+            var serviceProviderProfile = await _dbContext.ServiceProviders.FirstOrDefaultAsync(sp => sp.Email == email);
+            if (serviceProviderProfile != null)
             {
-                UserId = profile.ProfileId,
-                Type = profile.Type ?? "Non-user"
+                var sPResult = _SPPasswordHasher.VerifyHashedPassword(serviceProviderProfile, serviceProviderProfile.Password ?? "", password);
+                if (sPResult == PasswordVerificationResult.Success)
+                {
+                    return new User
+                    {
+                        UserId = serviceProviderProfile.ServiceProvidersId,
+                        Type = serviceProviderProfile.Type ?? "Non-User"
+                    };
+                }
+                else
+                {
+
+                    Console.WriteLine($"--------------spresult----------{sPResult} has failedddddddd");
+                    Console.WriteLine($"--------checking plaintext----------------");
+                    var pTP = await _dbContext.ServiceProviders.FirstOrDefaultAsync(sp => sp.Email == email);
+                    if (pTP != null)
+                    {
+                        return new User
+                        {
+                            UserId = serviceProviderProfile.ServiceProvidersId,
+                            Type = serviceProviderProfile.Type ?? "Non-User"
+                        };
+                    }
 
 
-            };
+                    return null;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"------------couldn't find a sp profile------------{serviceProviderProfile}-------");
 
-
-
+                return null;
+            }
         }
+        else
+        {
+            // Verify password
+            var result = _PatientPasswordHasher.VerifyHashedPassword(profile, profile.Password ?? "", password);
+            if (result == PasswordVerificationResult.Success)
+            {
+                return new User
+                {
+                    UserId = profile.ProfileId,
+                    Type = profile.Type ?? "Non-user"
 
-        return null;
+
+                };
+
+
+
+            }
+
+            return null;
+        }
+        // return null;
+
+
     }
 
 
